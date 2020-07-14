e2a88d4abeb625f1f07d90c0568e07976c6c1957
Wink Saville <wink@saville.com>
2020-07-14 12:07:52 -0700

    Looking at trailing edge anomalies

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Wing with ribs
 
 WIP
+
+nwing.py is the naca5305 airfoil which has anomalies at the trailing
+edge when fattened. Specifically there is a "notch". See best by comparing
+the follow two images, the other images show the TE from different perspectives.
+
+halfWingShell-nrmlTe-shell_+0.1-yAxis-3.png:
+![halfWingShell-nrmlTe-shell_+0.1-yAxis-3.png](./halfWingShell-nrmlTe-shell_+0.1-yAxis-3.png)
+
+halfWingShell-fatTe-shell_+0.1-yAxis-3.png:
+![halfWingShell-fatTe-shell_+0.1-yAxis-3.png](./halfWingShell-fatTe-shell_+0.1-yAxis-3.png)
--- nwing.py
+++ nwing.py
@@ -4,29 +4,23 @@ from fattenTe import fattenTe
 
 import cadquery as cq # type: ignore
 
-from typing import List, Sequence, Tuple
-
 chord: float = 50 
 h = 100
 
 # Normalize, Scale, fattenTe
-scaleFactor: float = 1/naca5305[0][0]
+scaleFactor = 1/naca5305[0][0]
 nNaca5305 = scaleListOfTuple(naca5305, scaleFactor)
-sNaca5305: List[Tuple[float, float]] = scaleListOfTuple(nNaca5305, chord)
-fNaca5305: List[Tuple[float, float]] = fattenTe(sNaca5305, 0.25, 10)
+sNaca5305 = scaleListOfTuple(nNaca5305, chord)
+#fNaca5305 = fattenTe(sNaca5305, 0.25, 10)
+fNaca5305 = sNaca5305
 
-airfoil = (
-    cq.Workplane("YZ")
-    .spline(fNaca5305).close()
-)
-
-halfWing = (
-    airfoil
-    .extrude(h)
-)
-#show_object(halfWing)
+# Create the 2D airfoil then extrude
+airfoil = cq.Workplane("YZ").spline(fNaca5305).close()
+halfWing = airfoil.extrude(h)
 
 # Shell the halfWing
-halfWingShell = halfWing.shell(-0.1)
-#show_object(halfWingShell)
+halfWingShell = halfWing.shell(0.1)
+show_object(halfWingShell)
+#show_object(halfWing)
+#show_object(airfoil)
 
