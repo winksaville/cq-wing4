82c481045990b147475ed2a6b61023d97e2689f2
Wink Saville <wink@saville.com>
2020-07-26 22:03:17 -0700

    Change tail to flat plates.
    The previous version was a V tail with naca0005 airfoil section but
    it didn't print very well and had to have a 0.40 shellThickness.

--- tail4.py
+++ tail4.py
@@ -8,59 +8,55 @@ from wing import Wing
 
 chord = 15
 span = 40
-incidenceAngle = 0
-dihedral = 45
-sweep = 0
-
-# Needs to be thick as it appears with the ribs it wants to
-# print two layers. At 0.25, 0.26, 0.3 prusa-slicer only printed
-# part of the LE and TE.
 tk = 0.40
 
-shortCut = False
-wing = Wing.makeWing(
-    airfoilSeq=naca0005,
-    chord=chord,
-    span=span,
-    incidenceAngle=incidenceAngle,
-    dihedral=dihedral,
-    sweep=sweep,
-    shellThickness=tk,
-    ribCount=0,
-    shortCut=shortCut,
-    ctx=globals(),
-)
-show(wing, ctx=globals())
+horzStabilizer = cq.Workplane("XY").box(chord, span, tk).translate((0, 0, tk / 2))
+# show(horzStabilizer, ctx=globals())
 
-cabinLength: float = chord
-cabinDiameter: float = 4.0
-cabinYOffset: float = 0.0
-cabinZOffset: float = 0  # -0.014
-cabin = (
+vertStabilizer = (
+    cq.Workplane("XY")
+    .box(chord, span / 2, tk)
+    .rotate((0, 0, 0), (1, 0, 0), 90)
+    .translate((0, 0, span / 4))
+)
+# show(vertStabilizer, ctx=globals())
+
+stabilizer = horzStabilizer.union(vertStabilizer)
+# show(stabilizer, ctx=globals())
+
+boomLength: float = chord
+boomDiameter: float = 4.0
+boomXOffset: float = chord / 2
+boomYOffset: float = 0
+boomZOffset: float = +boomDiameter / 2
+boom = (
     cq.Workplane("XZ")
-    .circle(cabinDiameter / 2)
+    .circle(boomDiameter / 2)
     .extrude(chord)
-    .rotate((0, 0, 0), (1, 0, 0), -90)
-    .translate((0, cabinYOffset, cabinZOffset))
+    .rotate((0, 0, 0), (0, 0, 1), -90)
+    .translate((boomXOffset, boomYOffset, boomZOffset))
 )
-# show(cabin, ctx=globals())
+# show(boom, ctx=globals())
 
-wingCabin = wing.union(cabin)
-# show(wingCabin, ctx=globals())
+tail = stabilizer.union(boom)
 
 c = RectCon(xLen=2.25, yLen=2.25, zLen=6)
+xCut = c.male.rotate((0, 0, 0), (0, 1, 0), 90).translate(
+    (-chord / 2, 0, boomDiameter / 2)
+)
+yCut = c.male.rotate((0, 0, 0), (0, 1, 0), 270).translate(
+    (chord / 2, 0, boomDiameter / 2)
+)
 # show(c.male, ctx=globals())
+# show(xCut, ctx=globals())
+# show(yCut, ctx=globals())
 
-wing4 = wingCabin.cut(
-    c.female.rotate((0, 0, 0), (1, 0, 0), 180).translate((0, cabinYOffset, chord))
-).cut(c.female.translate((0, cabinYOffset, cabinZOffset)))
-show(wing4, ctx=globals())
+tail4 = tail.cut(xCut).cut(yCut)
+show(tail4, ctx=globals())
 
 import io
 
 tolerance = 0.001
-f = io.open(
-    f"tail4-tol_{tolerance}-ch_{chord}-span_{span}-ia_{incidenceAngle}-di_{dihedral}-tk_{tk}.stl", "w+"
-)
-cq.exporters.exportShape(wing4, cq.exporters.ExportTypes.STL, f, tolerance)
+f = io.open(f"tail4-tol_{tolerance}-ch_{chord}-span_{span}-tk_{tk}.stl", "w+")
+cq.exporters.exportShape(tail4, cq.exporters.ExportTypes.STL, f, tolerance)
 f.close()
