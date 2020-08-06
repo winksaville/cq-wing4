e60f429e89e335f0d5b563bdd8f5044c952223fe
Wink Saville <wink@saville.com>
2020-08-06 15:07:39 -0700

    Add command line parameters
    Added -a --angle, -lp --linearPrecision, -ap --angularPrecision.

--- dowel.py
+++ dowel.py
@@ -1,17 +1,56 @@
+#!/usr/bin/env python3
+import argparse
+import math
+
 import cadquery as cq  # type: ignore
 
 from rectcon import RectCon
-from utils import setCtx, show
+from utils import dbg, setCtx, show
 
 setCtx(globals())
 
+dfltDowelAngleDegrees: float = 0
+dfltLinearPrecisionMm: float = 0.001
+dfltAngularPrecisionDegrees: float = 1
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-a",
+    "--dowelAngle",
+    help=f"angle in degrees of a kinked dowel, default={dfltDowelAngleDegrees}",
+    nargs="?",
+    type=float,
+    default=math.radians(dfltDowelAngleDegrees),
+)
+parser.add_argument(
+    "-lp",
+    "--linearPrecision",
+    help=f"linear precision in milli-meters of stl file, default={dfltLinearPrecisionMm}",
+    nargs="?",
+    type=float,
+    default=dfltLinearPrecisionMm,
+)
+parser.add_argument(
+    "-ap",
+    "--angularPrecision",
+    help=f"angular precision in degrees of stl file, default={dfltAngularPrecisionDegrees}",
+    nargs="?",
+    type=float,
+    default=math.radians(dfltAngularPrecisionDegrees),
+)
+args = parser.parse_args()
+
 # Create dowel
-c = RectCon(xLen=2.25, yLen=2.25, zLen=6, dowelAngle=0)
+c = RectCon(xLen=2.25, yLen=2.25, zLen=6, dowelAngle=args.dowelAngle)
 dowel = c.dowelHorz()
 show(dowel)
 
 import io
 
-tolerance = 0.001
-fname = f"dowel-tol_{tolerance:.4f}-x_{c.dowelBb.xlen:.4f}-y_{c.dowelBb.ylen:.4f}-z_{c.dowelBb.zlen:.4f}-a_{c.dowelAngle:.4f}.stl"
-cq.exporters.export(dowel, fname, tolerance=tolerance)
+linearPrecision: float = args.linearPrecision
+angularPrecision: float = args.angularPrecision
+fname = f"dowel-lp_{linearPrecision:.4f}-ap_{math.degrees(angularPrecision)}-x_{c.dowelBb.xlen:.4f}-y_{c.dowelBb.ylen:.4f}-z_{c.dowelBb.zlen:.4f}-a_{c.dowelAngle:.4f}.stl"
+dbg(f"fname={fname}")
+cq.exporters.export(
+    dowel, fname, tolerance=linearPrecision, angularPrecision=angularPrecision
+)
